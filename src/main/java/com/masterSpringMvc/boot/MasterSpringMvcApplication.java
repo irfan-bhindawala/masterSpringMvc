aab2cb235cdf376e0a31670a35971c9901b6fcee
irfan jaan <irfanjaan10@gmail.com>
2016-12-18 16:59:54 +0530

    hello world page commit

--- src/main/java/com/masterSpringMvc/boot/MasterSpringMvcApplication.java
+++ src/main/java/com/masterSpringMvc/boot/MasterSpringMvcApplication.java
@@ -1,9 +1,11 @@
-package com.example;
+package com.masterSpringMvc.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.masterSpringMvc.controller")
 public class MasterSpringMvcApplication {
 
 	public static void main(String[] args) {
